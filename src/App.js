52b84a88b788181400542e89d65355c1af97f671
Sheikh Rashed <mdrashed1310@gmail.com>
2021-02-15 00:37:54 +0600

    Implement UI in React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from 'react';
 import './App.css';
 
+// Components
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+
 function App() {
-    return <div className='App'></div>;
+    return (
+        <div className='App'>
+            <Header />
+            <div className='container'>
+                <Balance />
+                <IncomeExpense />
+                <TransactionList />
+                <AddTransaction />
+            </div>
+        </div>
+    );
 }
 
 export default App;
